9b8834595f39d018f80f930c9bf909c406178ebc
Sameer Kumar <sameerk1292@gmail.com>
2019-07-05 19:52:43 +0530

    Updated to test cli

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // Grab the provided args
 const [...args] = process.argv;
 
+console.log(`These are the arguments ${args}`);
+
 exports.printMsg = function() {
   console.log(`These are the arguments ${args}`);
 };
